10e10f6fbcfe0ff832713fb0aee853b3cd90149c
thewalla07 <thewalla07@gmail.com>
2017-02-21 14:50:24 +0000

    Added impl and tests of homomorphic addition, closes #16

---
+++ src/crypto/homomorphic.go
@@ -0,0 +1,33 @@
+package crypto
+
+import (
+	"math/big"
+)
+
+func (key *PrivateKey) AddCipherTexts(ciphertexts ...*big.Int) (total *big.Int, err error) {
+    total, err = key.PublicKey.AddCipherTexts(ciphertexts...)
+    return
+}
+
+// Accepts one or more ciphertexts and homomorphically sums them
+func (key *PublicKey) AddCipherTexts(ciphertexts ...*big.Int) (total *big.Int, err error) {
+
+    if err = key.validate(); err != nil {
+        return nil, err
+    }
+
+    // create an encryption of voting value zero to start off
+    zero := new(big.Int)
+    total, err = key.Encrypt(zero)
+    if err != nil {
+        return nil, err
+    }
+
+    // D(E(m1,r1).E(m2,r2) mod n^2) = m1 + m2 mod n
+    for _, ciphertext := range ciphertexts {
+        total = new(big.Int).Mul(total, ciphertext)
+        total.Mod(total, key.NSquared)
+    }
+
+    return total, nil
+}
---
+++ src/crypto/homomorphic_test.go
@@ -0,0 +1,58 @@
+package crypto_test
+
+import (
+	"github.com/CPSSD/voting/src/crypto"
+	"math/big"
+	"testing"
+)
+
+func TestHomomorphicProperties(t *testing.T) {
+
+	var tests = []struct {
+		input []*big.Int
+		correct *big.Int
+	}{
+        {[]*big.Int {big.NewInt(1), big.NewInt(1), big.NewInt(-123422), big.NewInt(-2341317)}, big.NewInt(2)},
+        {[]*big.Int {big.NewInt(0), big.NewInt(0),big.NewInt(1), big.NewInt(0)}, big.NewInt(1)},
+        {[]*big.Int {big.NewInt(1), big.NewInt(1),big.NewInt(1), big.NewInt(1)}, big.NewInt(4)},
+        {[]*big.Int {big.NewInt(0), big.NewInt(0),big.NewInt(0), big.NewInt(0)}, big.NewInt(0)},
+		{[]*big.Int {big.NewInt(799), big.NewInt(0),big.NewInt(201), big.NewInt(2)}, big.NewInt(1002)},
+	}
+
+	for _, c := range tests {
+		got, err := CheckHomomorphicAddition(c.input...)
+		if got.Cmp(c.correct) != 0 {
+			t.Error("For input", c.input, "expected", c.correct, "got", got, "with error", err, "\n")
+		}
+	}
+}
+
+func CheckHomomorphicAddition(inputs ...*big.Int) (total *big.Int, err error) {
+
+	priv, err := crypto.GenerateKeyPair(512)
+	if err != nil {
+		return nil, err
+	}
+
+    zero := new(big.Int)
+    total, err = priv.Encrypt(zero)
+    if err != nil {
+        return nil, err
+    }
+
+    // create encryptions of each input, and add the ciphertexts incrementally
+    for _, input := range inputs {
+        ciphertext, err := priv.Encrypt(input)
+        if err != nil {
+            return nil, err
+        }
+        total, err = priv.AddCipherTexts(ciphertext, total)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    // decrypt the total of the ciphertexts
+    total, err = priv.Decrypt(total)
+    return total, err
+}
