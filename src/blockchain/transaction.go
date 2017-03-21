a7ed4698b2b6041739c78990e33c6f315de9e7b4
thewalla07 <thewalla07@gmail.com>
2017-03-21 18:16:15 +0000

    Added transaction.go, can generate and check proof of work.

---
+++ src/blockchain/transaction.go
@@ -0,0 +1,77 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+    "encoding/gob"
+    "encoding/hex"
+    "strings"
+    "bytes"
+    "time"
+	"fmt"
+)
+
+type Transaction struct {
+	Header    TransactionHeader
+	Ballot    []byte
+	Signature []byte
+    proof     []byte
+}
+
+type TransactionHeader struct {
+	VoteToken  []byte // so that we know what token is authorizing the vote
+	BallotSize uint32 // in case we lose track of the size of our ballot
+    BallotHash [32]byte // hash of the ballot to tie it to the header
+    Timestamp  uint32 // timestamp so we know when to count this vote for
+	Nonce      uint32 // the incremented value for proof of work
+}
+
+func New(token, ballot []byte) (t *Transaction) {
+	t = &Transaction{
+		Header: TransactionHeader{
+			VoteToken: token,
+		},
+		Ballot: ballot,
+	}
+
+	t.Header.BallotSize = uint32(len(t.Ballot))
+	t.Header.BallotHash = sha256.Sum256(t.Ballot)
+	t.Header.Timestamp = uint32(time.Now().Unix())
+    t.Header.Nonce = uint32(0)
+
+	return t
+}
+
+func (t *Transaction) createProof(prefixLen int) (proof uint32) {
+
+    // We need the first prefixLen characters of the hex
+    // representation of hash(t) to be equal to 0 for our proof
+    // to be valid.
+
+    altT := t
+    prefix := strings.Repeat("0", prefixLen)
+
+    for {
+        var data []byte
+        var b bytes.Buffer
+        enc := gob.NewEncoder(&b)
+        err := enc.Encode(&altT.Header)
+        if err != nil {
+            panic(err)
+        }
+        data = b.Bytes()
+
+        hash := sha256.Sum256(data)
+        if checkProof(prefix, prefixLen, hash) {
+            s := hex.EncodeToString(hash[:])
+            fmt.Printf("Nonce: %v\nHash: %s\n\n",altT.Header.Nonce, s)
+            break
+        }
+        altT.Header.Nonce++
+    }
+    return altT.Header.Nonce
+}
+
+func checkProof(prefix string, len int, hash [32]byte) bool {
+    s := hex.EncodeToString(hash[:])
+    return s[:len] == prefix
+}
