4953aad5cdc3df29185b90feace30c9bcff9d2b5
thewalla07 <thewalla07@gmail.com>
2017-05-20 19:59:09 +0100

    Implemented tallying of votes; closes #45

--- src/election/ballot.go
+++ src/election/ballot.go
@@ -1,10 +1,11 @@
 package election
 
 import (
+	"errors"
 	"fmt"
-    "strings"
+	"github.com/CPSSD/voting/src/crypto"
 	"math/big"
-    "errors"
+	"strings"
 )
 
 var (
@@ -12,9 +13,9 @@ var (
 )
 
 type Ballot struct {
-	VoteToken     string                   // VT of the voter who owns ballot
-	NumSelections int                      // number of selections in the ballot
-	Selections         []Selection // list of selections on the ballot
+	VoteToken     string      // VT of the voter who owns ballot
+	NumSelections int         // number of selections in the ballot
+	Selections    []Selection // list of selections on the ballot
 }
 
 type Selection struct {
@@ -25,65 +26,114 @@ type Selection struct {
 
 type Format struct {
 	NumSelections int
-	Selections []Selection
+	Selections    []Selection
 }
 
 func (b *Ballot) Fill(f Format, vt string) (err error) {
-    if len(f.Selections) != f.NumSelections {
-        return InvalidFormatError
-    }
+	if len(f.Selections) != f.NumSelections {
+		return InvalidFormatError
+	}
 
-    b.VoteToken = vt
-    b.NumSelections = f.NumSelections
-    b.Selections = make([]Selection, f.NumSelections)
+	b.VoteToken = vt
+	b.NumSelections = f.NumSelections
+	b.Selections = make([]Selection, f.NumSelections)
 
-    for i, s := range f.Selections {
-        fmt.Printf("Enter your selection (0 or 1) for Candidate %v: ", s.Name)
-        var input int
-        fmt.Scanf("%v\n", &input)
+	for i, s := range f.Selections {
+		fmt.Printf("Enter your selection (0 or 1) for Candidate %v: ", s.Name)
+		var input int
+		fmt.Scanf("%v\n", &input)
 
-        vote := big.NewInt(int64(input))
+		vote := big.NewInt(int64(input))
 
-        selection := Selection{
-            Name: s.Name,
-            Vote: vote,
-            Proof: make([]byte, 0),
-        }
+		selection := Selection{
+			Name:  s.Name,
+			Vote:  vote,
+			Proof: make([]byte, 0),
+		}
 
-        b.Selections[i] = selection
-    }
+		b.Selections[i] = selection
+	}
 
-    // TODO: let user review inputs before returning
+	// TODO: let user review inputs before returning
 
 	return nil
 }
 
 func CreateFormat() (f *Format) {
-    fmt.Printf("How many selections are on the ballot? ")
-    var input int
-    fmt.Scanf("%v\n", &input)
+	fmt.Printf("How many selections are on the ballot? ")
+	var input int
+	fmt.Scanf("%v\n", &input)
 
-    f = &Format{
-        NumSelections: input,
-        Selections: make([]Selection, input),
-    }
+	f = &Format{
+		NumSelections: input,
+		Selections:    make([]Selection, input),
+	}
 
-    fmt.Println("Use double quotes for description entries")
-    for i := 0 ; i < input ; i++ {
-        fmt.Printf("Enter user description for selection %v: ", i+1)
-        var desc string
+	fmt.Println("Use double quotes for description entries")
+	for i := 0; i < input; i++ {
+		fmt.Printf("Enter user description for selection %v: ", i+1)
+		var desc string
 
-        fmt.Scanf("%q\n", &desc)
-        fmt.Println("You entered:",desc)
+		fmt.Scanf("%q\n", &desc)
+		fmt.Println("You entered:", desc)
 
-        desc = strings.Trim(desc, " \n")
+		desc = strings.Trim(desc, " \n")
 
-        s := Selection{
-            Name: desc,
-        }
+		s := Selection{
+			Name: desc,
+		}
 
-        f.Selections[i] = s
-    }
+		f.Selections[i] = s
+	}
 
-    return f
+	return f
+}
+
+type Tally struct {
+	Totals map[string]*big.Int
+}
+
+func (f *Format) Tally(bs *[]Ballot, key *crypto.PrivateKey) (t *Tally, err error) {
+
+	t = &Tally{
+		Totals: make(map[string]*big.Int, 0),
+	}
+
+	selectionCounts := make(map[string][]*big.Int, 0)
+
+	for _, s := range f.Selections {
+		selectionCounts[s.Name] = make([]*big.Int, len(*bs))
+	}
+
+	for i, b := range *bs {
+		for _, s := range b.Selections {
+			if _, ok := selectionCounts[s.Name]; ok {
+				selectionCounts[s.Name][i] = s.Vote
+			}
+		}
+	}
+
+	// TODO: decrypt each sub tally
+	for name, count := range selectionCounts {
+		sum, err := key.AddCipherTexts(count...)
+		if err != nil {
+			fmt.Println("Error summing ciphertexts!!!")
+			panic(err)
+		}
+
+		fmt.Println("One summed ciphertext, decrypting...")
+
+		result, err := key.Decrypt(sum)
+		if err != nil {
+			fmt.Println("Error decrypting a ciphertext!!!")
+			panic(err)
+		}
+
+		fmt.Println("One ciphertext decrypted...")
+		fmt.Println(name, result.String())
+
+		t.Totals[name] = result
+	}
+
+	return t, err
 }
